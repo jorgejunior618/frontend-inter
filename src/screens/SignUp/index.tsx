29f68a1705659acae35986a8559622708bc43c8c
jorgejunior618 <jorgejunior618@gmail.com>
2021-12-09 20:50:44 -0300

    Tela de SignUp

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,12 +1,52 @@
-import React from "react";
+import {
+  Background,
+  ButtonGroup,
+  InputGroup,
+  Wrapper,
+} from './styles'
+import Card from '../../components/Card';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import backgroundImage from '../../assets/images/background-login.jpg'
+import logoInter from '../../assets/images/Inter-orange.png'
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const completeSignUp = () => {
+    navigate('/dashboard');
+  }
   return (
-    <div>
-      <h1>
-        SignUp
-      </h1>
-    </div>
+    <Wrapper>
+      <Background image={backgroundImage}/>
+
+      <Card width="403px">
+        <img
+          src={logoInter}
+          width={172}
+          alt="Logotipo Banco Inter"
+        />
+
+        <InputGroup>
+          <Input placeholder="NOME" type="text" />
+          <Input placeholder="SOBRENOME" type="text" />
+          <Input placeholder="EMAIL" type="email" />
+          <Input placeholder="SENHA" type="password" />
+          <Input placeholder="CONFIRMAR SENHA" type="password" />
+        </ InputGroup>
+
+        <ButtonGroup>
+          <Button type="button" onClick={completeSignUp}>
+            Cadastrar
+          </Button>
+          <p>
+            Já tem uma conta? <Link to="/signin">Entre já</Link>
+          </p>
+        </ ButtonGroup>
+      </ Card>
+    </ Wrapper>
   );
 }
 
